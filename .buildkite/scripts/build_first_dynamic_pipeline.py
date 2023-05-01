dca150f02b7f101d3998efe3fdcaaea70a8c1432
ldnicolasmay <ldnicolasmay@gmail.com>
2023-04-30 21:14:59 -0400

    bypass utils module difficulties

--- .buildkite/scripts/build_first_dynamic_pipeline.py
+++ .buildkite/scripts/build_first_dynamic_pipeline.py
@@ -1,6 +1,20 @@
+from typing import Any
+
 import yaml
 
-from utils.utils import format_dict_strs
+# from utils.utils import format_dict_strs
+
+
+def format_dict_strs(thing: Any, **kwargs) -> Any:
+    match thing:
+        case str():
+            return thing.format(**kwargs)
+        case list():
+            return [format_dict_strs(t, **kwargs) for t in thing]
+        case dict():
+            return {k.format(**kwargs) if isinstance(k, str) else k: format_dict_strs(v, **kwargs) for k, v in thing.items()}
+        case _:
+            return thing
 
 
 plugins_dict = {
